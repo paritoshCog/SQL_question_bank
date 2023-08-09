e9ca3babcd952ed2394c4e567058bfc958333312
Paritosh Prasad <128465174+paritoshCog@users.noreply.github.com>
2023-08-09 23:40:26 +0530

    Update solutions.sql

--- solutions.sql
+++ solutions.sql
@@ -1,1 +1,16 @@
+1-> SELECT LocationName,StateSiteID FROM Location where LocationCode IN (SELECT LocationCode
+    FROM Location
+    MINUS
+    SELECT LocationCode
+    FROM LocationAttributes);
 
+ 
+
+2-> SELECT LocationName,RestaurantClass,SiteID,StoreOpenEffectiveDate,LocationFullName FROM Location l JOIN LocationAttributes la ON l.LocationCode=la.locationCode;
+
+ 
+
+ 
+
+4->ALTER TABLE Person ADD InactiveDate DATETIME;
+  DELECT FROM InactiveDate WHERE (EXTRACT(YEAR from SYSDATE)-EXTRACT(YEAR from InactiveDate))>6;
