8cc7b87b5d9575e0eb3f5ced81fb5b8debf63c5d
Paritosh Prasad <128465174+paritoshCog@users.noreply.github.com>
2023-08-10 00:37:05 +0530

    Update solutions.sql

--- solutions.sql
+++ solutions.sql
@@ -1,16 +1,20 @@
-1-> SELECT LocationName,StateSiteID FROM Location where LocationCode IN (SELECT LocationCode
-    FROM Location
-    MINUS
-    SELECT LocationCode
-    FROM LocationAttributes);
+-- qes 1
+SELECT LocationName,StateSiteID FROM Location where LocationCode IN (SELECT LocationCode
+FROM Location
+MINUS
+SELECT LocationCode
+FROM LocationAttributes);
 
  
-
-2-> SELECT LocationName,RestaurantClass,SiteID,StoreOpenEffectiveDate,LocationFullName FROM Location l JOIN LocationAttributes la ON l.LocationCode=la.locationCode;
-
- 
+-- qes 2
+SELECT LocationName,RestaurantClass,SiteID,StoreOpenEffectiveDate,LocationFullName FROM Location l JOIN LocationAttributes la ON l.LocationCode=la.locationCode;
 
  
+-- qes 3
+SELECT REGEXP_REPLACE(FirstName,'[!-+,@,^,_,0-9]','') as FirstName,REGEXP_REPLACE(LastName,'[!-+,^,_,@,0-9]','') as LastName from Person;
 
-4->ALTER TABLE Person ADD InactiveDate DATETIME;
-  DELECT FROM InactiveDate WHERE (EXTRACT(YEAR from SYSDATE)-EXTRACT(YEAR from InactiveDate))>6;
+-- qes 4
+ALTER TABLE Person ADD InactiveDate DATETIME;
+DELECT FROM InactiveDate WHERE (EXTRACT(YEAR from SYSDATE)-EXTRACT(YEAR from InactiveDate))>6;
+
+-- qes 5
